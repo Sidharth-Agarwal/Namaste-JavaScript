1e37bb3102bdb49150b7a7b5c444a27bd171c369
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-23 11:55:24 +0530

    Data Hiding

---
+++ 33DataHiding.js
@@ -0,0 +1,21 @@
+function counter()
+{
+    var count = 0;
+    return function increamentCounter()
+    {
+        count++;
+        console.log(count);
+    }
+}
+
+var counter1 = counter();
+counter1();
+counter1();
+
+// This is a new counter and will not affect another counter
+var counter2 = counter();
+counter2();
+counter2();
+counter2();
+counter2();
+counter2();
