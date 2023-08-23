861180eee5f0e40d74490d35d10b6128710f601b
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-23 12:08:30 +0530

    Constructor Function

---
+++ 34ConstructorFunction.js
@@ -0,0 +1,23 @@
+// Example of constructor function
+function counter()
+{
+    var count = 0;
+
+    this.incrementCounter = function()
+    {
+        count++;
+        console.log(count);
+    }
+
+    this.decrementCounter = function()
+    {
+        count--;
+        console.log(count);
+    }
+}
+
+// New keyword is used while using constructor function
+var counter1 = new counter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.decrementCounter();
