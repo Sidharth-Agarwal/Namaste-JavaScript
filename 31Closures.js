94095700eed7e48c784a8b615302e0e207a136f2
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-23 11:42:19 +0530

    Good Example of Closure

---
+++ 31Closures.js
@@ -0,0 +1,21 @@
+function outest()
+{
+    var c = 20;
+    function outer(b)
+    {
+        function inner()
+        {
+            console.log()
+            {
+                console.log(a,b,c);
+            }
+        }
+        let a = 10;
+        return inner;
+    }
+    return outer;
+}
+
+// let a = 100;
+var close = (outest())("Hellow");
+close();
