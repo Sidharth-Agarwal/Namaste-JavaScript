4b8d794d40b1752a1be1903583fabbe4bd69d3f5
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-06 20:55:40 +0530

    Shortest Program Possible

--- 7ShortestJavaScriptProgram.js
+++ 7ShortestJavaScriptProgram.js
@@ -4,3 +4,18 @@
 
 // It is also creates a "this" object for us
 // It points to the window object in the global level
+
+// 'let' & 'const' keyword in not recognized by the window object
+// only 'var' is recognized by the window object
+// In any other case undefined is shown
+var a = 10;
+
+function x() {
+    const x = 100;
+}
+
+console.log(window.a);
+console.log(this.a)
+console.log(a);
+console.log(window);
+console.log(this === window);
