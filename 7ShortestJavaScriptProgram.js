6d501f0cc50a65b9c223e654781128637cdf50db
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-06 20:44:03 +0530

    Shortest Program in JS

--- 7ShortestJavaScriptProgram.js
+++ 7ShortestJavaScriptProgram.js
@@ -1,3 +1,6 @@
 // This is nothing but the shortest program possible in JavaScript
 
 // Type "window" in the console to check everything
+
+// It is also creates a "this" object for us
+// It points to the window object in the global level
