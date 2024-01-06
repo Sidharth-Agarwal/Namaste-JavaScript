5e9d7e5d906afecc81e9e0d21a697d074d6e5822
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-06 18:55:35 +0530

    Hoisting in JS

---
+++ 3HoistingInJavaScript.js
@@ -0,0 +1,11 @@
+getName();
+console.log(x);
+console.log(getName);
+
+var x = 7;
+
+// Here we have created an arrow function
+// Here it acts like a variable
+var getName = () =>{
+    console.log("Namaste JavaScript!!");
+}
