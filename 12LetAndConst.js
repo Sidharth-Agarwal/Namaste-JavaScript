2f63da75c74a910a037fe6380c2b1f5116ea2233
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-01-11 21:29:20 +0530

    let and const in JavaScript

---
+++ 12LetAndConst.js
@@ -0,0 +1,10 @@
+let a = 10;
+console.log(a);
+// let a = 1000; -> This can not be done in the case of let
+var b = 10;
+var b = 100;
+console.log(b);
+
+const c = 1000;
+// const c; -> Even this is not allowed in the case of const
+// const c = 100 -> This too
