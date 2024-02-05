88b89a5b54a812ea5fa15047c4c1b73de63196ad
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2024-02-05 10:42:00 +0530

    Closures

--- 27setTimeOut.js
+++ 27setTimeOut.js
@@ -1,10 +1,9 @@
-function x()
-{
-    var i = 1;
-    setTimeout(function()
-    {
-        console.log(i);
-    },1000);
-    console.log("Hellew There ;)")
+function x(){
+    for(var i = 1; i <= 5; i++){
+        setTimeout(function(){
+            console.log(i);
+        },i*1000)
+    }
+    console.log("Hie...!");
 }
 x();
--- 28setTimeout.js
+++ 28setTimeout.js
@@ -1,12 +1,9 @@
-function x()
-{
-    for(var i = 1; i <= 5; i++)
-    {
+function x(){
+    for(let i = 1; i <= 5; i++){
         setTimeout(function(){
             console.log(i);
         },i*1000)
     }
-
     console.log("Hie...!");
 }
 x();
